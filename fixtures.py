feff9e71f65e5547730611cb00cc14b184b23359
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-02-22 10:25:34 +0000

    more ideas

--- fixtures.py
+++ fixtures.py
@@ -1,3 +1,16 @@
 import pytest
 
 
+@pytest.fixture #(scope='session')
+def use_me():
+    import pdb; pdb.set_trace()
+    # heavily generated data
+    return {'k1': 'val'}
+
+
+def test_dic_check(use_me):
+    assert len(use_me) == 1
+
+
+def test_dic_check_second(use_me):
+    assert list(use_me.keys()) == ['k1']
